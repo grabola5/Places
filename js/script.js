27fecc6c4da19c7aa69ce4d7ddac3c4c019518ba
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-07-27 16:24:38 +0200

    Add options to carousel

--- js/script.js
+++ js/script.js
@@ -2,22 +2,19 @@
 (function(){
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
-  // options
   cellAlign: 'left',
-  contain: true
-});
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-  hash: true,
+  contain: true,
+  hash: true
 });
 
 var restartBtn = document.querySelector('.restart-btn');
 restartBtn.addEventListener ('click', function () {
   	flkty.select('0');
 });
+var progressBar = document.querySelector('.progress-bar')
 
-
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
 })();
