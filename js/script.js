9d9658144839acb4ce8a958be609466d3a9fc793
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-07-28 22:46:06 +0200

    Add map with one marker

--- js/script.js
+++ js/script.js
@@ -30,4 +30,14 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+window.initMap = function () {
+var cracow = {lat: 50.063410, lng: 19.945157};
+var map = new google.maps.Map (
+	document.getElementById('map'), {zoom: 4, center: cracow});
+var marker = new google.maps.Marker ({position: cracow, map: map});
+};
+
+initMap ();
+
 })();
