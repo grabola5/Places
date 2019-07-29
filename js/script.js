3387f0028d075afe465b0c8e612ca006b967c3d7
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-07-30 00:19:34 +0200

    Clean code

--- js/script.js
+++ js/script.js
@@ -2,29 +2,30 @@
 (function(){
 
 var templateItem = document.getElementById('template-cell').innerHTML;
-Mustache.parse(templateItem);
 var listSlides = '';
+var results = document.getElementById('main-carousel');
+var elem = document.querySelector('.main-carousel');
+var restartBtn = document.querySelector('.restart-btn');
+var progressBar = document.querySelector('.progress-bar')
+
+Mustache.parse(templateItem);
 
 for (var i=0; i < slides.length; i++) {
 	console.log(slides);
 	listSlides += Mustache.render (templateItem, slides[i]);
 };
 
-var results = document.getElementById('main-carousel');
 results.insertAdjacentHTML ('beforeend', listSlides);
 
-var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   	cellAlign: 'left',
   	contain: true,
   	hash: true
 });
 
-var restartBtn = document.querySelector('.restart-btn');
 restartBtn.addEventListener ('click', function () {
   	flkty.select('0');
 });
-var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
   	progress = Math.max( 0, Math.min( 1, progress ) );
@@ -35,24 +36,27 @@ window.initMap = function () {
 	var cuba = slides[0].coords;
 
 	var map = new google.maps.Map (document.getElementById('map'), {
-	zoom: 6,
-	center: cuba
-});
+		zoom: 6,
+		center: cuba
+		}
+	);
 
 	for (let i=0; i<slides.length; i++) {
-	var localization = slides[i].coords;
-	var marker = new google.maps.Marker ({
-		position: localization,
-		map: map,
-		index: i
-	});
-	marker.addListener('click', function () {
-  	flkty.select(i);
-});
-}
+		var localization = slides[i].coords;
+		var marker = new google.maps.Marker ({
+			position: localization,
+			map: map,
+			index: i
+			}
+		);
+		marker.addListener('click', function () {
+  			flkty.select(i);
+		});
+	}	
+	
 	flkty.on( 'change', function (index) {
 		map.panTo(slides[index].coords);
-	}
-); 
+	}); 
 };
+
 })();
